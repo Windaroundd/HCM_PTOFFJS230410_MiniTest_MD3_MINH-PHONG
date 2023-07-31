95665b1e6e60d56bc108e025a7f6105d6ea78430
Phong Huynh <phonghqm@gmail.com>
2023-07-31 21:36:09 +0700

    update

--- middlewares/todo.middleware.js
+++ middlewares/todo.middleware.js
@@ -5,14 +5,24 @@ let todosData = JSON.parse(fs.readFileSync(todoPath, "utf-8"));
 
 module.exports = {
   checkExist: (req, res, next) => {
+    const METHOD = req.method;
     const { id } = req.params;
     const { title } = req.body;
     let indexTodo = todosData.findIndex((todo) => todo.id == id);
     let indexTitle = todosData.findIndex((todo) => todo.title == title);
-    if (indexTodo == -1) {
-      res.json({ message: "Todo not found”" });
-    } else if (indexTitle == -1) {
-      es.json({ message: "Todo already exists" });
+    // if (indexTodo == -1 && id !== undefined) {
+    //   res.json({ message: "Todo not found”" });
+    // } else if (indexTitle == -1 && title !== undefined) {
+    //   res.json({ message: "Todo already exists" });
+    // }
+    if (METHOD == "GET" || METHOD == "PUT" || METHOD == "DELETE") {
+      if (indexTodo == -1) {
+        res.json({ message: "Todo not found”" });
+      }
+    } else if (METHOD == "POST") {
+      if (indexTitle == -1 && title !== undefined) {
+        res.json({ message: "Todo already exists" });
+      }
     }
     next();
   },
